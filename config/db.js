5d2e019e2cab7bcb27367c9e184ba23c0338b6e2
jake67wood <jakewood6788@gmail.com>
2025-03-13 15:10:48 -0600

    Update db.js
    updated

--- config/db.js
+++ config/db.js
@@ -2,11 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const connection = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL || "postgresql://root:gMUN1iwijZKZxdKiCSPDeFaZ6hCptK54@dpg-cv9j4b5ds78s73bplvt0-a/secrets_wdgc",
+  ssl: {
+    rejectUnauthorized: false, // Necesario para Render
+  },
 });
 
 module.exports = connection;
