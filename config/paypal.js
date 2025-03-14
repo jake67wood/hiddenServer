8ebbdf79815a6c3d6483f82f822538e31467e069
jake67wood <jakewood6788@gmail.com>
2025-03-14 09:02:55 -0600

    Update paypal.js

--- config/paypal.js
+++ config/paypal.js
@@ -1,9 +1,18 @@
 const { config } = require('dotenv')
 config()
 
-export const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT
-export const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET
-export const PAYPAL_API = process.env.PAYPAL_API
-export const PORT = process.env.PORT
-export const HOST = process.env.HOST
-export const HOST_FRONTEND = process.env.HOST_FRONTEND
+const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT
+const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET
+const PAYPAL_API = process.env.PAYPAL_API
+const PORT = process.env.PORT
+const HOST = process.env.HOST
+const HOST_FRONTEND = process.env.HOST_FRONTEND
+
+module.exports = {
+  PAYPAL_API_CLIENT,
+  PAYPAL_API_SECRET,
+  PAYPAL_API,
+  PORT,
+  HOST,
+  HOST_FRONTEND
+}
