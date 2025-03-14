e662ac6fef1ca1809a17bba0c51b13233dd94fe5
jake67wood <jakewood6788@gmail.com>
2025-03-14 10:34:16 -0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -9,7 +9,12 @@ const User = {
             const query = 'INSERT INTO users (name, last_name, phone,address, email, city,country, password, is_paid, package, price, date_today, token) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *';
             const values = [userData.name, userData.last_name, userData.phone, userData.address, userData.email, userData.city, userData.country, userData.password, 0, 'null','0','date','token'];
 
-            const res = await db.query(query, values);
+            try {
+                const res = await db.query(query, values);
+                console.log('user registered');
+            } catch (error){
+                console.log('user error',error.message);
+            }
         });
     }
 };
