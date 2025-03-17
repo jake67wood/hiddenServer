a045c1db3030e538bf161055db5cfa422b1e7cde
jake67wood <jakewood6788@gmail.com>
2025-03-17 12:53:06 -0600

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,40 +39,13 @@ exports.register = (req, res) => {
 
 };
 exports.session = async (req,res)=>{
-
-      const sessionId = req.cookies['connect.sid']; // Obtén el ID de la sesión desde la cookie
-
-  if (!sessionId) {
-    return res.status(401).json({ error: 'No autorizado' });
-  }
-
-  try {
-    // Busca la sesión en la base de datos
-    const result = await db.query(
-      'SELECT sess FROM session WHERE sid = $1 AND expire > NOW()',
-      [sessionId]
-    );
-
-    if (result.rows.length === 0) {
-      return res.status(401).json({ error: 'Sesión inválida o expirada' });
-    }
-
-    // Obtén los datos de la sesión
-    const sessionData = result.rows[0].sess;
-    req.user = sessionData.user; // Guarda el usuario en el objeto `req`
-    next(); // Continúa con la siguiente función
-  } catch (error) {
-    console.error('Error al verificar sesión:', error);
-    res.status(500).json({ error: 'Error en el servidor' });
-  }
-    /* 
     if (req.session.user) {
         console.log('checar sesion')
         res.status(200).json({user: req.session.user})
     } else {
         console.log('no sesion')
         res.status(401).json({message: 'Not autheticated'})
-    } */
+    }
 }
 exports.logout = (req,res)=>{
     req.session.destroy((err)=>{
