8c240cc99c2aee7abf4e7788f3fa7e0d9cbad0d6
jake67wood <jakewood6788@gmail.com>
2025-03-17 13:54:09 -0600

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,12 @@ exports.login = async (req, res) => {
     if (!passwordMatch) {
         res.status(401).json({ message: 'Invalid username or password' });
     }else{
-        req.session.user = { username }
+        
+    await db.query(
+      'UPDATE users SET is_logged_in = TRUE WHERE id = $1',
+      [user.id]
+    );
+        /* req.session.user = { username } */
         res.status(200).json({ message: 'Login successful',user: req.session.user });
     }
 };
@@ -39,22 +44,73 @@ exports.register = (req, res) => {
 
 };
 exports.session = async (req,res)=>{
-    if (req.session.user) {
+  const { username } = req.params;
+
+  try {
+    // Busca al usuario en la base de datos
+    const userResult = await pool.query(
+      'SELECT * FROM users WHERE email = $1',
+      [username]
+    );
+
+    if (userResult.rows.length === 0) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    const user = userResult.rows[0];
+
+    // Responde con el estado de la sesión
+    res.status(200).json({ user: user.is_logged_in });
+  } catch (error) {
+    console.error('Error al verificar sesión:', error);
+    res.status(500).json({ message: 'Error en el servidor' });
+  }
+    
+/*     if (req.session.user) {
         console.log('checar sesion')
         res.status(200).json({user: req.session.user})
     } else {
         console.log('no sesion')
         res.status(401).json({message: 'Not autheticated'})
-    }
+    } */
 }
 exports.logout = (req,res)=>{
-    req.session.destroy((err)=>{
+
+      const { username } = req.body;
+
+  try {
+    // Busca al usuario en la base de datos
+    const userResult = await db.query(
+      'SELECT * FROM users WHERE email = $1',
+      [username]
+    );
+
+    if (userResult.rows.length === 0) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    const user = userResult.rows[0];
+
+    // Actualiza el campo is_logged_in a FALSE
+    await db.query(
+      'UPDATE users SET is_logged_in = FALSE WHERE id = $1',
+      [user.id]
+    );
+
+    // Responde con un mensaje de éxito
+    res.status(200).json({ message: 'Logout exitoso' });
+  } catch (error) {
+    console.error('Error en el logout:', error);
+    res.status(500).json({ message: 'Error en el servidor' });
+  }
+    
+/*     req.session.destroy((err)=>{
         if (err) {
             return res.status(500).json({message: 'Invalid logout'})
         }
         res.clearCookie('connect.sid')
         res.status(200).json({message: 'Logout session successfully'})
-    })
+    }) */
 }
 exports.dashboardInfo = async (req,res)=>{
     const { username } = req.query.us // it's username not user | username is in login and local state
